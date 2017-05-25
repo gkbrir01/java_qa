7b4586669b020f08a78b236261aeb62a09f86961
Grzegorz Kozlowski <gkbrir01@gmail.com>
2017-05-25 20:53:53 +0200

    Work 3
    Work 3

---
+++ sandbox/src/test/java/pl/stqa/pft/sandbox/PointTests.java
@@ -0,0 +1,14 @@
+package pl.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests {
+
+  @Test
+  public void testDistance(){
+     Point p1 = new Point(10,15);
+     Point p2 = new Point(5,10);
+    Assert.assertEquals(p1.distance(p1,p2), 7.0710678118654755);
+   }
+}
