e91862b24783d6cc9d6dc8a28ebab0f39892be08
Grzegorz Kozlowski <gkbrir01@gmail.com>
2017-05-26 12:59:06 +0200

    Work 4 version1
    Recorded tests for adding a group and contact

---
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/ContactCreationTests.java
@@ -0,0 +1,80 @@
+package pl.stqa.pft.addressbook;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+import java.util.concurrent.TimeUnit;
+import java.util.Date;
+import java.io.File;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+import static org.openqa.selenium.OutputType.*;
+
+public class  ContactCreationTests{
+    FirefoxDriver wd;
+    
+    @BeforeMethod
+    public void setUp() throws Exception {
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    }
+    
+    @Test
+    public void ContactCreationTests() {
+        wd.get("http://10.94.11.4/addressbook/");
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys("admin");
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys("secret");
+        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+        wd.findElement(By.linkText("add new")).click();
+        wd.findElement(By.name("firstname")).click();
+        wd.findElement(By.name("firstname")).clear();
+        wd.findElement(By.name("firstname")).sendKeys("Grzegorz2");
+        wd.findElement(By.name("lastname")).click();
+        wd.findElement(By.name("lastname")).clear();
+        wd.findElement(By.name("lastname")).sendKeys("Kozlowski2");
+        wd.findElement(By.name("address")).click();
+        wd.findElement(By.name("address")).clear();
+        wd.findElement(By.name("address")).sendKeys("Warsaw, ul. Magiera 3/22");
+        wd.findElement(By.name("home")).click();
+        wd.findElement(By.name("home")).clear();
+        wd.findElement(By.name("home")).sendKeys("+484083699");
+        wd.findElement(By.name("mobile")).click();
+        wd.findElement(By.name("mobile")).clear();
+        wd.findElement(By.name("mobile")).sendKeys("+48601000000");
+        wd.findElement(By.name("email")).click();
+        wd.findElement(By.name("email")).clear();
+        wd.findElement(By.name("email")).sendKeys("gkbrir04@gmail.com");
+        if (!wd.findElement(By.xpath("//div[@id='content']/form/select[1]//option[16]")).isSelected()) {
+            wd.findElement(By.xpath("//div[@id='content']/form/select[1]//option[16]")).click();
+        }
+        if (!wd.findElement(By.xpath("//div[@id='content']/form/select[2]//option[3]")).isSelected()) {
+            wd.findElement(By.xpath("//div[@id='content']/form/select[2]//option[3]")).click();
+        }
+        wd.findElement(By.name("byear")).click();
+        wd.findElement(By.name("byear")).clear();
+        wd.findElement(By.name("byear")).sendKeys("1995");
+        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+    }
+    
+    @AfterMethod
+    public void tearDown() {
+        wd.quit();
+    }
+    
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}
--- sandbox/src/test/java/pl/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/pl/stqa/pft/sandbox/PointTests.java
@@ -6,11 +6,11 @@ import org.testng.annotations.Test;
 public class PointTests {
 
   //Work3
-  
+
   @Test
   public void testDistance(){
      Point p1 = new Point(10,15);
      Point p2 = new Point(5,10);
-    Assert.assertEquals(p1.distance(p1,p2), 7.0710678118654755);
+     Assert.assertEquals(p1.distance(p1,p2), 7.0710678118654755);
    }
 }
