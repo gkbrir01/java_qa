07ff4a2fc67f519b5d3a78902de12c5a4317b1ee
Grzegorz Kozlowski <gkbrir01@gmail.com>
2017-06-09 13:02:09 +0200

    l4_m08-find+max-pl
    l4_m08-find+max-pl

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/model/GroupData.java
@@ -1,7 +1,8 @@
 package pl.stqa.pft.addressbook.model;
 
 public class GroupData {
-  private final String id;
+
+  private int id;
   private final String name;
   private final String header;
   private final String footer;
@@ -9,20 +10,21 @@ public class GroupData {
 
 
   public GroupData(String name, String header, String footer) {
-    this.id = null;
+    this.id = 0;
     this.name = name;
     this.header = header;
     this.footer = footer;
   }
 
-  public GroupData(String id, String name, String header, String footer) {
+  public GroupData(int id, String name, String header, String footer) {
     this.id = id;
     this.name = name;
     this.header = header;
     this.footer = footer;
   }
 
-  public String getId() {
+
+  public int getId() {
     return id;
   }
 
@@ -36,6 +38,10 @@ public class GroupData {
 
   public String getFooter() {
     return footer;
+  }
+
+  public void setId(int id) {
+    this.id = id;
   }
 
   @Override
@@ -53,14 +59,15 @@ public class GroupData {
 
     GroupData groupData = (GroupData) o;
 
-    if (id != null ? !id.equals(groupData.id) : groupData.id != null) return false;
+    if (id != groupData.id) return false;
     return name != null ? name.equals(groupData.name) : groupData.name == null;
   }
 
   @Override
   public int hashCode() {
-    int result = id != null ? id.hashCode() : 0;
+    int result = id;
     result = 31 * result + (name != null ? name.hashCode() : 0);
     return result;
   }
+
 }
