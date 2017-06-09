a7d37ba907b3b3578d7fff52b70b637ed381d766
Grzegorz Kozlowski <gkbrir01@gmail.com>
2017-06-09 14:30:26 +0200

    l4_m10-collection+sorting-pl Last
    l4_m10-collection+sorting-pl Last

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -27,11 +27,6 @@ public class GroupDeletionTests extends TestBase {
     Assert.assertEquals(after.size(), before.size() - 1);
 
     before.remove(before.size() - 1);
-    //Moja wstawka
-    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
-    before.sort(byId);
-    after.sort(byId);
-    //-----------------------------
     Assert.assertEquals(before, after);
   }
 
