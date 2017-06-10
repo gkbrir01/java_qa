7468c550712793014fcd8585b2b7ed718f2f4a5e
Grzegorz Kozlowski <gkbrir01@gmail.com>
2017-06-10 15:16:13 +0200

    l4_m06-collection+comparison-pl with contacts
    l4_m06-collection+comparison-pl with contacts

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -12,7 +12,7 @@ public class ContactCreationTests extends TestBase{
   public void testContactCreation() {
     app.getNavigationHelper().gotoHomePage();
     List<ContactData> before = app.getContactHelper().getContactList();
-    app.getContactHelper().createContact(new ContactData("Grzegorz10", "Kozlowski4", "Warsaw, ul. Magiera 3/22", null, "+48601000000", "gkbrir05@gmail.com","test1"));
+    app.getContactHelper().createContact(new ContactData("Grzegorz", "Kozlowski", "Warsaw, ul. Magiera 3/22", null, "+48601000000", "gkbrir05@gmail.com","test1"));
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() + 1);
   }
