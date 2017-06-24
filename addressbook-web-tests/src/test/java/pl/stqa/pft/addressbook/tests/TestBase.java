613b3db07644431454c4953a324aa86d6b25025d
Grzegorz Kozlowski <gkbrir01@gmail.com>
2017-06-25 00:20:54 +0200

    l6_m10+-+Test+stand+configuration
    l6_m10+-+Test+stand+configuration

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/TestBase.java
@@ -1,18 +1,14 @@
 package pl.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import pl.stqa.pft.addressbook.appmanager.ApplicationManager;
 
-/**
- * Created by gkozlowski on 2017-05-26.
- */
 public class TestBase {
 
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.IE);
+  protected static final ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeSuite
   public void setUp() throws Exception {
