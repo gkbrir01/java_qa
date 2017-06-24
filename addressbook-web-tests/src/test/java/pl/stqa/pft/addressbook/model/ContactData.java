62e12d2747c60c042f13596edbc4806ab611f80f
Grzegorz Kozlowski <gkbrir01@gmail.com>
2017-06-24 20:09:51 +0200

    Work 14
    Generators test data

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/model/ContactData.java
@@ -1,20 +1,31 @@
 package pl.stqa.pft.addressbook.model;
 
-import java.io.File;
+import com.google.gson.annotations.Expose;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
 
+import java.io.File;
+@XStreamAlias("contact")
 public class ContactData {
+  @XStreamOmitField
   private int id = Integer.MAX_VALUE;
+  @Expose
   private String firstName;
+  @Expose
   private String lastName;
+  @Expose
   private String address;
   private String homePhone;
+  @Expose
   private String mobilePhone;
   private String workPhone;
   private String allPhones;
+  @Expose
   private String email;
   private String email2;
   private String email3;
   private String allEmail;
+  @Expose
   private String group;
   private File photo;
 
