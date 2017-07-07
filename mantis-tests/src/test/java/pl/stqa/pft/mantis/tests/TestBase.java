9f75c0ba8359171cc965b908e0d58b67ac8ad544
Grzegorz Kozlowski <gkbrir01@gmail.com>
2017-07-07 10:32:34 +0200

    l8_m5-ftp
    l8_m5-ftp

--- mantis-tests/src/test/java/pl/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/pl/stqa/pft/mantis/tests/TestBase.java
@@ -5,6 +5,9 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import pl.stqa.pft.mantis.appmanager.ApplicationManager;
 
+import java.io.File;
+import java.io.IOException;
+
 public class TestBase {
 
   protected static final ApplicationManager app
@@ -13,10 +16,12 @@ public class TestBase {
   @BeforeSuite
   public void setUp() throws Exception {
     app.init();
+    app.ftp().upload(new File("mantis-tests/src/test/resources/config_defaults_inc.php"), "config_defaults_inc.php", "config_defaults_inc.bak");
   }
 
   @AfterSuite(alwaysRun = true)
-  public void tearDown() {
+  public void tearDown() throws IOException {
+    app.ftp().restore("config_defaults_inc.bak", "config_defaults_inc.php");
     app.stop();
   }
 }
